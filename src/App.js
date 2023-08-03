dc72ad631348cb278071c825a552f94f59a19b01
Ronalds Borisovs <ronaldsborisovs@MacBook-Air-Ronalds.local>
2023-08-03 12:25:36 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
@@ -17,9 +17,10 @@ function App() {
         >
           Learn React
         </a>
+        <p>Github</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
